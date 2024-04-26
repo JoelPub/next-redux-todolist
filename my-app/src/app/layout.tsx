474bad5f8b02afa2c95a87e779b788b27b1dd607
joelrhino <FICTITIOUS@TUTAMAIL.COM>
2024-04-26 08:55:05 +0000

    init

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+import ReduxProvider from "@/redux/provider";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
+    </html>
+  );
+}
